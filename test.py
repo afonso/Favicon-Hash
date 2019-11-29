0834fb84be61a3fa0b6393b470d68a1fac2ff2f9
Afonso <afonso@yack.com.br>
2019-11-29 18:30:17 -0300

    afd

--- test.py
+++
@@ -1,9 +0,0 @@
-import sys, requests, base64, mmh3
-
-response = requests.get('https://www.juul.com/favicon.ico')
-data = response.content
-#data = binascii.b2a_base64(data)
-data = base64.encodebytes(data)
-data = mmh3.hash(data)
-#sys.stdout.buffer.write(data)
-print(data)
