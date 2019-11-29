4305d38438f866940fd99637b82c1ce4d13598e3
Afonso <afonso@yack.com.br>
2019-11-29 16:41:43 -0300

    asd

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+import os, mmh3, sys, requests
 from flask import Flask, jsonify, request
 
 
@@ -8,8 +8,16 @@ app = Flask(__name__)
 def main():
     if request.headers.get('Authorization') == '42':
         return jsonify({"42": "a resposta para a vida, o universo e tudo mais"})
-    return jsonify({"message": "Não entre em pânico!"})
+    return jsonify({"usage": "/get?favicon=url.com/fav.ico"})
 
+@app.route('/get')
+def gethash():
+    url = request.args.get('favicon') 
+    req = requests.get(url)
+    favicon = req.content.encode('base64')
+    hash = mmh3.hash(favicon)
+    return jsonfy({"http.favicon.hash": str(hash)})
+    
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
