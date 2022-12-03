539fd5c4aae83830ebdc6f190f7749d84fd2ae70
Afonso <afonso@yack.com.br>
2022-12-03 22:22:21 +0000

    fly deploy

--- main.py
+++ main.py
@@ -13,7 +13,19 @@ def gethash():
     req = requests.get(url, verify=False)
     favicon = base64.encodebytes(req.content)
     hash = mmh3.hash(favicon)
-    return "http.favicon.hash:" + str(hash)
+    query = "http.favicon.hash:" + str(hash)
+    shodan = requests.get('https://api.shodan.io/shodan/host/search?key='+ os.environ.get("SHODAN_KEY") +'&query='+ query)
+    result = shodan.json()
+    entries = "<pre>query: " + query
+    entries += "\ntotal: " + str(result["total"])
+    
+    if result["total"] > 0:
+        entries += "\nresult:"
+        for entry in result["matches"]:
+            entries += "\n"
+            entries += entry["ip_str"]
+
+    return entries + "</pre>"
     
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
